8a26a51258cff31d61ad6c886250a1eb2e3c8ebb
mariogodinez <eldelabrisa1@hotmail.com>
2017-08-22 19:19:06 -0500

    deleted config

--- config.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-	email: 'futureweb2000@gmail.com',
-	pass: 'dweb2000frontend',
-	port: process.env.PORT || 2000,
-	SECRET_TOKEN: process.env.TOKEN_SECRET || "tokenultrasecreto",
-	db: process.env.MONGODB_URI || 'mongodb://localhost:27017/tya'
-}
